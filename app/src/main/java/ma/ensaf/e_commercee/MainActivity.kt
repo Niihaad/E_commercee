eb702a4cd29c2226266337dec44b4b6c341c7dbd
Niihaad <110766469+Niihaad@users.noreply.github.com>
2023-10-24 09:07:45 +0100

    Initial commit

---
+++ app/src/main/java/ma/ensaf/e_commercee/MainActivity.kt
@@ -0,0 +1,11 @@
+package ma.ensaf.e_commercee
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
